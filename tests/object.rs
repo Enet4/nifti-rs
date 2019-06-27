be327f2ee182692bbd598f5d61d73f671614e994
Eduardo Pinho <enet4mikeenet@gmail.com>
2019-06-27 18:03:23 +0100

    Add more tests for streamed objects

--- tests/object.rs
+++ tests/object.rs
@@ -6,8 +6,8 @@ extern crate nifti;
 extern crate pretty_assertions;
 
 use nifti::{
-    Endianness, InMemNiftiObject, NiftiHeader, NiftiObject, NiftiType, NiftiVolume,
-    RandomAccessNiftiVolume, XForm,
+    Endianness, InMemNiftiObject, StreamedNiftiObject, NiftiHeader, NiftiObject, NiftiType,
+    NiftiVolume, RandomAccessNiftiVolume, XForm,
 };
 
 #[test]
@@ -28,6 +28,30 @@ fn minimal_nii_gz() {
 
     const FILE_NAME: &str = "resources/minimal.nii.gz";
     let obj = InMemNiftiObject::from_file(FILE_NAME).unwrap();
+    assert_eq!(obj.header(), &minimal_hdr);
+    let volume = obj.volume();
+    assert_eq!(volume.data_type(), NiftiType::Uint8);
+    assert_eq!(volume.dim(), [64, 64, 10].as_ref());
+}
+
+#[test]
+fn streamed_minimal_nii_gz() {
+    let minimal_hdr = NiftiHeader {
+        sizeof_hdr: 348,
+        dim: [3, 64, 64, 10, 0, 0, 0, 0],
+        datatype: 2,
+        bitpix: 8,
+        pixdim: [0., 3., 3., 3., 0., 0., 0., 0.],
+        vox_offset: 352.,
+        scl_slope: 0.,
+        scl_inter: 0.,
+        magic: *b"n+1\0",
+        endianness: Endianness::Big,
+        ..Default::default()
+    };
+
+    const FILE_NAME: &str = "resources/minimal.nii.gz";
+    let obj = StreamedNiftiObject::from_file(FILE_NAME).unwrap();
     assert_eq!(obj.header(), &minimal_hdr);
     let volume = obj.volume();
     assert_eq!(volume.data_type(), NiftiType::Uint8);
@@ -59,6 +83,30 @@ fn minimal_nii() {
 }
 
 #[test]
+fn streamed_minimal_nii() {
+    let minimal_hdr = NiftiHeader {
+        sizeof_hdr: 348,
+        dim: [3, 64, 64, 10, 0, 0, 0, 0],
+        datatype: 2,
+        bitpix: 8,
+        pixdim: [0., 3., 3., 3., 0., 0., 0., 0.],
+        vox_offset: 352.,
+        scl_slope: 0.,
+        scl_inter: 0.,
+        magic: *b"n+1\0",
+        endianness: Endianness::Big,
+        ..Default::default()
+    };
+
+    const FILE_NAME: &str = "resources/minimal.nii";
+    let obj = StreamedNiftiObject::from_file(FILE_NAME).unwrap();
+    assert_eq!(obj.header(), &minimal_hdr);
+    let volume = obj.volume();
+    assert_eq!(volume.data_type(), NiftiType::Uint8);
+    assert_eq!(volume.dim(), [64, 64, 10].as_ref());
+}
+
+#[test]
 fn minimal_by_hdr() {
     let minimal_hdr = NiftiHeader {
         sizeof_hdr: 348,
@@ -76,6 +124,30 @@ fn minimal_by_hdr() {
 
     const FILE_NAME: &str = "resources/minimal.hdr";
     let obj = InMemNiftiObject::from_file(FILE_NAME).unwrap();
+    assert_eq!(obj.header(), &minimal_hdr);
+    let volume = obj.volume();
+    assert_eq!(volume.data_type(), NiftiType::Uint8);
+    assert_eq!(volume.dim(), [64, 64, 10].as_ref());
+}
+
+#[test]
+fn streamed_minimal_by_hdr() {
+    let minimal_hdr = NiftiHeader {
+        sizeof_hdr: 348,
+        dim: [3, 64, 64, 10, 0, 0, 0, 0],
+        datatype: 2,
+        bitpix: 8,
+        pixdim: [0., 3., 3., 3., 0., 0., 0., 0.],
+        vox_offset: 0.,
+        scl_slope: 0.,
+        scl_inter: 0.,
+        magic: *b"ni1\0",
+        endianness: Endianness::Big,
+        ..Default::default()
+    };
+
+    const FILE_NAME: &str = "resources/minimal.hdr";
+    let obj = StreamedNiftiObject::from_file(FILE_NAME).unwrap();
     assert_eq!(obj.header(), &minimal_hdr);
     let volume = obj.volume();
     assert_eq!(volume.data_type(), NiftiType::Uint8);
@@ -192,6 +264,54 @@ fn f32_nii_gz() {
     assert_eq!(volume.get_f32(&[0, 0, 5]).unwrap(), 0.);
     assert_eq!(volume.get_f32(&[5, 0, 4]).unwrap(), 0.4);
     assert_eq!(volume.get_f32(&[0, 8, 5]).unwrap(), 0.8);
+}
+
+#[test]
+fn streamed_f32_nii_gz() {
+    let f32_hdr = NiftiHeader {
+        sizeof_hdr: 348,
+        dim: [3, 11, 11, 11, 1, 1, 1, 1],
+        datatype: 16,
+        bitpix: 32,
+        pixdim: [1., 1., 1., 1., 1., 1., 1., 1.],
+        vox_offset: 352.,
+        scl_slope: 1.,
+        scl_inter: 0.,
+        srow_x: [1., 0., 0., 0.],
+        srow_y: [0., 1., 0., 0.],
+        srow_z: [0., 0., 1., 0.],
+        sform_code: 2,
+        magic: *b"n+1\0",
+        endianness: Endianness::Little,
+        ..Default::default()
+    };
+
+    const FILE_NAME: &str = "resources/f32.nii.gz";
+    let obj = StreamedNiftiObject::from_file(FILE_NAME).unwrap();
+    assert_eq!(obj.header(), &f32_hdr);
+
+    assert_eq!(obj.header().sform().unwrap(), XForm::AlignedAnat);
+
+    let volume = obj.into_volume();
+    assert_eq!(volume.data_type(), NiftiType::Float32);
+    assert_eq!(volume.dim(), [11, 11, 11].as_ref());
+    assert_eq!(volume.slice_dim(), &[11, 11]);
+
+    let slices: Vec<_> = volume
+        .map(|r| r.expect("slice construction should not fail"))
+        .collect();
+
+    for slice in &slices {
+        assert_eq!(slice.data_type(), NiftiType::Float32);
+        assert_eq!(slice.dim(), &[11, 11]);
+    }
+
+    assert_eq!(slices[5].get_f32(&[5, 5]).unwrap(), 1.);
+    assert_eq!(slices[0].get_f32(&[5, 0]).unwrap(), 0.);
+    assert_eq!(slices[0].get_f32(&[0, 5]).unwrap(), 0.);
+    assert_eq!(slices[5].get_f32(&[0, 0]).unwrap(), 0.);
+    assert_eq!(slices[4].get_f32(&[5, 0]).unwrap(), 0.4);
+    assert_eq!(slices[5].get_f32(&[0, 8]).unwrap(), 0.8);
 }
 
 #[test]
